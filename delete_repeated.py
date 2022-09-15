a6245d30ba244cba7090e6952e528c3149a07e4c
Matias Ringeling Selman <matias.ringeling@mail.udp.cl>
2022-09-15 10:18:08 -0300

    deletead unused lines

--- delete_repeated.py
+++ delete_repeated.py
@@ -6,5 +6,18 @@ def delete_repeated_lines():
             for line in lines_set:
                 f.write(line)
 
+
+# delete lines with blank urls
+def delete_blank_lines():
+    with open('user-ct-test-collection-09.txt', 'r') as f:
+        lines = f.readlines()
+        with open('user-ct-test-collection-09_2.txt', 'w') as f:
+            for line in lines:
+                splited_line = line.split('\t')
+                #if splited line 4 is not blank
+                if splited_line[4] != '\n':
+                    f.write(line)
+
 if __name__ == '__main__':
     delete_repeated_lines()
+    delete_blank_lines()
